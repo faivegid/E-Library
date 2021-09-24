bb63cab6708186b3a3360e4246ef94237c5036e4
Praise Balogun <praise.balogun@decagon.dev>
2021-09-24 13:58:59 +0100

    modification to the admin page

--- GBLAC.MVC/wwwroot/js/admin.js
+++ GBLAC.MVC/wwwroot/js/admin.js
@@ -4,6 +4,8 @@ const dash3 = document.querySelector('.dash2')
 const dash4 = document.querySelector('.dash4')
 const adminDetails = document.querySelector('.admin-details')
 const books = document.querySelector('.books')
+const usersTable = document.querySelector('.users-table')
+const veiwUserButton = document.querySelector('.view-users-button')
 
 dash1.addEventListener('click', function () {
     dash1.classList.remove('dash')
@@ -11,6 +13,8 @@ dash1.addEventListener('click', function () {
     dash3.classList.add('dash');
     adminDetails.classList.remove('hide')
     books.classList.add('hide')
+    modal.classList.add('hide')
+    usersTable.classList.add('hide')
 })
 dash2.addEventListener('click', function () {
     dash1.classList.add('dash');
@@ -18,16 +22,33 @@ dash2.addEventListener('click', function () {
     dash2.classList.remove('dash')
     adminDetails.classList.add('hide')
     books.classList.remove('hide')
+    modal.classList.add('hide')
+    usersTable.classList.add('hide')
 })
 
 const addButton = document.querySelector('.add-button')
 const modal = document.querySelector('.modal')
 const clostButton = document.querySelector('.close-button')
 const gridContainer = document.querySelector('.grid-container')
+
+
 addButton.addEventListener('click', function () {
     modal.classList.remove('hide')
-    gridContainer.style.opacity = '.3'
+    usersTable.classList.add('hide')
 })
+
+veiwUserButton.addEventListener('click', function () {
+    usersTable.classList.remove('hide')
+    dash1.classList.add('dash');
+    dash3.classList.add('dash');
+    dash2.classList.add('dash')
+    adminDetails.classList.add('hide')
+    books.classList.add('hide')
+    modal.classList.add('hide')
+
+})
+
+
 clostButton.addEventListener('click', function () {
     modal.classList.add('hide')
     gridContainer.style.opacity = '1'
